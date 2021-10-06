ecb9ed3995e5959c11d47f298edb2f06c0e8e9bf
Ebu Bekir Celik <ebu.celik@stud.fh-campuswien.ac.at>
2021-10-06 18:24:31 +0200

    project start

---
+++ Pods/Target Support Files/Pods-Lobbi/Pods-Lobbi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lobbi : NSObject
+@end
+@implementation PodsDummy_Pods_Lobbi
+@end
---
+++ Pods/Target Support Files/Pods-LobbiTests/Pods-LobbiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LobbiTests : NSObject
+@end
+@implementation PodsDummy_Pods_LobbiTests
+@end
